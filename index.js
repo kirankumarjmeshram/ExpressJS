cea067911fdac4021ee000afa38dc86822c75d97
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-12-24 17:07:22 +0530

    inline middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(logger);
+//app.use(oneMorelogger);
 //REST API
 //get => to get item from server
 //post =>
@@ -8,19 +9,35 @@ app.use(logger);
 //delete =>
 
 app.get("/",(req,res)=>{
-    return res.send({name:"googke.com"})
+    console.log("Route Handler")
+    return res.send({name:req.name})
+})
+//app.use(oneMorelogger);
+
+//inline middleaware
+app.get("/users",oneMorelogger,(req,res)=>{
+    return res.send("All users")
 })
 
-app.get("/users",(req,res)=>{
+app.get("/abc",(req,res)=>{
     return res.send("All users")
 })
 
 //middleware
 function logger(req,res,next){
+    req.name ="Kirankumar"
     console.log("one");
-    console.log("two");
-    next();//jump to app.use() and read lines after app.use()
+    console.log("Before");
+    next();//jump to net line of app.use() and read lines after app.use()
+    console.log("After")
 }
+
+function oneMorelogger(req,res,next){
+    console.log("Before One More");
+    next();
+    console.log("After One More")
+}
+
 
 app.listen(5555,()=>{
     console.log("Listening on port 5555")
